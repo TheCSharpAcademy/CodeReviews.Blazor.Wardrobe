ab61bdb53b2dd8ddd2f20e6a51feb4ce9d77f926
mantelli96 <mantelli96@libero.it>
2024-03-04 20:25:53 +0100

    test

---
+++ Buutyful.Wardrobe/Buutyful.Wardrobe/Program.cs
@@ -0,0 +1,33 @@
+using Buutyful.Wardrobe.Client.Pages;
+using Buutyful.Wardrobe.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Buutyful.Wardrobe.Client._Imports).Assembly);
+
+app.Run();
