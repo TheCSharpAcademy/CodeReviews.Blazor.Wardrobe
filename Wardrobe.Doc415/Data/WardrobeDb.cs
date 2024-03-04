b8cf2712b03505e409f89d5d0bb7a1362a695855
doc415 <sipahioglu_serdar@yahoo.com>
2024-03-04 23:42:41 +0300

    Finished project

---
+++ Wardrobe.Doc415/Data/WardrobeDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wardrobe.Doc415.Models;
+
+namespace Wardrobe.Doc415.Data;
+
+public class WardrobeDb: DbContext
+{
+    public WardrobeDb (DbContextOptions<WardrobeDb> options) : base(options) { }
+
+    public DbSet<Cloth> Clothes { get; set; }
+}
